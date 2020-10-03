0e5022a1803377523086b2d207fbc19a813096ef
mynamaneet <70784280+mynamaneet@users.noreply.github.com>
2020-10-02 19:41:56 -0700

    Update to ModLoader 0.1.19

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-java -jar .\source\DoLModLoader.jar
+java -cp .\source\DoLModLoader.jar -Djava.security.manager -Djava.security.policy=permissions.policy com.mynamaneet.dolmodloader.ModLoader
 
 pause
 exit
